270768889f818bc970098a7cf6fb5e5bb028f3cb
Samarrao <66645768+Samarrao@users.noreply.github.com>
2021-12-11 21:01:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let pics = 8;
+let pic;
+
+for(i=0;i<pics;i++){
+  pic = 1+i;
+document.getElementById('portfolio').innerHTML += "<img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' />";
+}
